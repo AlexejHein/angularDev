2daf45e9604257c8fffea0a68a438c6b1591db36
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-09-19 19:49:17 +0200

    add basiks

--- 01/src/app/app.component.ts
+++ 01/src/app/app.component.ts
@@ -7,13 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '01';
-  name = 'Alexej';
+  name = 'alexej';
   imgURL = 'https://angular.io/assets/images/logos/angular/angular.png';
+  images = [
+    'https://angular.io/assets/images/logos/angular/angular.png',
+    'https://angular.io/assets/images/logos/angular/angular_solidBlack.png',
+    ];
+  isDisabled = false;
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 20.2;
+  pizza = {
+    title: 'Carbonara',
+    description: 'Classic Italian pizza',
+    isVegetarian: false,
+    ingredients: ['Cheese', 'Tomato', 'Bacon']
+  };
+
+  blueClass = false;
+  fontSize = 16;
+
+
 
   getName(){
     return this.name;
   }
   changeImagr(e: KeyboardEvent){
     this.imgURL = (e.target as HTMLInputElement).value;
+  }
+  logImg(event: string){
+    console.log(event);
+  }
+
+  
+  hallo(){
+    setTimeout(() => {
+    this.name = 'bob';}, 2000);
+    setTimeout(() => {
+      this.name = 'Alexej';}, 4000);
   }
 }
--- 01/src/app/post/post.component.ts
+++ 01/src/app/post/post.component.ts
@@ -1,10 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, 
+         Input,
+         EventEmitter, 
+         Output, 
+         OnInit, OnChanges, DoCheck, SimpleChanges, AfterContentInit, 
+         AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
-  @Input() postImg = '';
+export class PostComponent implements  OnInit, OnChanges, 
+DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input('img') postImg = '';
+  @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() { 
+    console.log('constructor() called', this.postImg);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit() called', this.postImg);
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck() called');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges() called', changes);
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit() called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked() called');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit() called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked() called');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy() called');
+  }
 }
