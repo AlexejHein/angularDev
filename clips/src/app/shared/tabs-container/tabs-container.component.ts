45e4fdae0d29fb89f6b282b4d6234ecff6e8322b
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-09-21 19:21:01 +0200

    add Tabs

---
+++ clips/src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, AfterContentInit, ContentChild } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChild(TabComponent) tabs = {};
+  constructor(){}
+  ngAfterContentInit(): void {
+    
+  }
+}
