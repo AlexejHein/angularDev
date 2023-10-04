03a221a76e2ae5c172d81bb339c0dedf1e7163c3
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-10-04 20:28:57 +0200

    add edit clip

--- clips/src/app/models/clip.model.ts
+++ clips/src/app/models/clip.model.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 export default interface IClip {
-  uid: string;
+    docID?: string;
+    uid: string;
     displayName: string;
     title: string;
     fileName: string;
--- clips/src/app/video/manage/manage.component.ts
+++ clips/src/app/video/manage/manage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ClipService } from '../../services/clip.service';
+import IClip from '../../models/clip.model';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -9,12 +12,26 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 export class ManageComponent implements OnInit {
 
   videoOrder = '1';
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  clips: IClip[] = [];
+  activeClip: IClip | null = null;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
+  ) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
     });
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = []
+      docs.forEach(doc => {
+        this.clips.push({docID: doc.id, ...doc.data()
+        })
+      })
+    })
   }
 
   sort(event: Event) {
@@ -23,5 +40,12 @@ export class ManageComponent implements OnInit {
     this.router.navigate([], {
       relativeTo: this.route, queryParams: {sort: value}
     }).then(r => console.log(r));
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault();
+
+    this.activeClip = clip;
+    this.modal.toggleModal('editClip');
   }
 }
